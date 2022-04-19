4e59ae1f46d462ec4adaab57fac98991ff470a3f
BillBenon <61540696+BillBenon@users.noreply.github.com>
2022-04-19 14:00:00 +0100

    creating models

--- level_two/level_two_app/admin.py
+++ level_two/level_two_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from level_two_app.models import AccessRecord, Topic, WebPage
 
 # Register your models here.
+admin.site.register(AccessRecord)
+admin.site.register(Topic)
+admin.site.register(WebPage)
