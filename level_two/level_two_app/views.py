4f8309a352d5f4ce812b03250db2b9b34adf274d
BillBenon <61540696+BillBenon@users.noreply.github.com>
2022-04-22 09:31:39 +0200

    Models-Templates-Views paradigm

--- level_two/level_two_app/views.py
+++ level_two/level_two_app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from level_two_app.models import Topic, WebPage, AccessRecord
 
 
 # Create your views here.
 def index(request):
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpages_list}
     my_dict = {'insert_content': "HELLO! I AM FROM LEVEL TWO"}
-    return render(request, 'level_two_app/index.html', context=my_dict)
+    return render(request, 'level_two_app/index.html', context=date_dict)
