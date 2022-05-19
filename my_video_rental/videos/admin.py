90f1cc46b3954a004df157e51b5d2c4eff7ec4bb
BillBenon <61540696+BillBenon@users.noreply.github.com>
2022-05-19 11:03:18 +0200

    feat: list editable view

--- my_video_rental/videos/admin.py
+++ my_video_rental/videos/admin.py
@@ -11,6 +11,10 @@ class MovieAdmin(admin.ModelAdmin):
 
     list_filter = ['release_year', 'length', 'title']
 
+    list_display = ['title', 'release_year', 'length']
+
+    list_editable = ['release_year', 'length']
+
 
 admin.site.register(models.Customer)
 admin.site.register(models.Movie, MovieAdmin)
